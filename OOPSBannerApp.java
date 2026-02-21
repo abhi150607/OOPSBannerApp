44a951f8eb82d6480ea693ca3ef04070303d757d
abhi150607 <ad3621@srmist.edu.in>
2026-02-22 00:46:33 +0530

    [Abhinav] Added Java file

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,7 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
 
-        System.out.println();
-
+        System.out.println();        
         System.out.println(String.join("   ","   *****   ","   *****   ","  *******  ","   ******    "));
         System.out.println(String.join("   "," **     ** "," **     ** ","  **     **"," **      **  "));
         System.out.println(String.join("   ","**       **","**       **","  **      **","**          "));
